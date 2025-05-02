5ac414904c8b550e4ac611375642a80c661fc44b
tobi <tobi19.py@gmail.com>
2025-05-02 14:17:36 -0500

    Create a command registry

--- repl.go
+++ repl.go
@@ -7,20 +7,63 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var commandRegistry map[string]cliCommand
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage: ")
+
+	for _, com := range commandRegistry {
+		name := com.name
+		des := com.description
+		fmt.Printf("%s: %s\n", name, des)
+	}
+
+	return nil
+}
+
+func commandExit() error {
+	fmt.Print("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func init() {
+	commandRegistry = map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
+
 func startRepl() {
 	input := bufio.NewScanner(os.Stdin)
 
 	for {
 		fmt.Print("Pokedex > ")
-		var command string
 		input.Scan()
-		words := cleanInput(input.Text())
-		if len(words) == 0 {
+		line := cleanInput(input.Text())
+		if len(line) == 0 {
 			continue
 		}
-		command = words[0]
-
-		fmt.Printf("Your command was: %s\n", command)
+		word := line[0]
+		if command, exists := commandRegistry[word]; exists == true {
+			command.callback()
+		}
+		fmt.Print("Unknown command\n")
 	}
 }
 
