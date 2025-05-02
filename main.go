7daf51f793318bf55e2ef29cd966764ab805afa1
tobi <tobi19.py@gmail.com>
2025-05-02 13:11:53 -0500

    Moved repl logic to startRepl method

--- main.go
+++ main.go
@@ -1,22 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	input := bufio.NewScanner(os.Stdin)
-
-	for {
-		fmt.Print("Pokedex > ")
-		var command string
-		if input.Scan() == true {
-			command = input.Text()
-		}
-		cInput := cleanInput(command)
-
-		fmt.Printf("Your command was: %s\n", cInput[0])
-	}
+	startRepl()
 }
--- repl.go
+++ repl.go
@@ -1,8 +1,28 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func startRepl() {
+	input := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		var command string
+		input.Scan()
+		words := cleanInput(input.Text())
+		if len(words) == 0 {
+			continue
+		}
+		command = words[0]
+
+		fmt.Printf("Your command was: %s\n", command)
+	}
+}
 
 func cleanInput(text string) []string {
 	newText := strings.TrimSpace(strings.ToLower(text))
