16f0da3debd642dc84a42773468fa8210c61eac1
tobi <tobi19.py@gmail.com>
2025-05-06 12:44:15 -0500

    Create internal pokeapi client to handle requests and responses. This encapsulates the map command from the client

---
+++ internal/pokeapi/location_list.go
@@ -0,0 +1,38 @@
+package pokeapi
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+// ListLocations -
+func (c *Client) ListLocations(pageURL *string) (LocationAreaResponse, error) {
+	url := baseURL + "/location-area"
+	if pageURL != nil {
+		url = *pageURL
+	}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return LocationAreaResponse{}, err
+	}
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return LocationAreaResponse{}, err
+	}
+	defer resp.Body.Close()
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return LocationAreaResponse{}, err
+	}
+
+	areas := LocationAreaResponse{}
+	if err = json.Unmarshal(data, &areas); err != nil {
+		return LocationAreaResponse{}, err
+	}
+
+	return areas, nil
+}
