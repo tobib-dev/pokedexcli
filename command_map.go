98e3f69cd0c6a42f4ae77d7ac8b9c6042fd63aac
tobi <tobi19.py@gmail.com>
2025-05-06 13:04:59 -0500

    Get response from client using config pointer and print locations

--- command_map.go
+++ command_map.go
@@ -1,71 +1,41 @@
 package main
 
 import (
-	"encoding/json"
+	"errors"
 	"fmt"
-	"io"
-	"net/http"
 )
 
-func commandMap(cfg *config) error {
-	var apiURL string
-	if len(cfg.next) == 0 {
-		apiURL = "https://pokeapi.co/api/v2/location-area/"
-	} else {
-		apiURL = cfg.next
-	}
-
-	res, err := http.Get(apiURL)
-	if err != nil {
-		return fmt.Errorf("Error retrieving http response")
-	}
-	defer res.Body.Close()
-
-	data, err := io.ReadAll(res.Body)
+func commandMapf(cfg *config) error {
+	areas, err := cfg.pokeapiClient.ListLocations(cfg.nextLocationsURL)
 	if err != nil {
 		return err
 	}
 
-	var areas LocationAreaResponse
-	if err = json.Unmarshal(data, &areas); err != nil {
-		return fmt.Errorf("Error unmarshalling response to struct")
-	}
-	cfg.next = areas.Next
+	cfg.nextLocationsURL = areas.Next
+	cfg.prevLocationsURL = areas.Previous
 
-	for _, area := range areas.Results {
-		fmt.Printf("%s\n", area.Name)
+	for _, loc := range areas.Results {
+		fmt.Println(loc.Name)
 	}
 
 	return nil
 }
 
 func commandMapb(cfg *config) error {
-	var apiURL string
-
-	if len(cfg.previous) == 0 {
-		apiURL = "https://pokeapi.co/api/v2/location-area/"
-	} else {
-		apiURL = cfg.previous
+	if cfg.prevLocationsURL == nil {
+		return errors.New("you are on the first page")
 	}
 
-	res, err := http.Get(apiURL)
-	if err != nil {
-		return err
-	}
-	defer res.Body.Close()
-
-	data, err := io.ReadAll(res.Body)
+	areas, err := cfg.pokeapiClient.ListLocations(cfg.prevLocationsURL)
 	if err != nil {
 		return err
 	}
 
-	var areas LocationAreaResponse
-	if err = json.Unmarshal(data, &areas); err != nil {
-		return err
-	}
+	cfg.nextLocationsURL = areas.Next
+	cfg.prevLocationsURL = areas.Previous
 
-	for _, area := range areas.Results {
-		fmt.Printf("%s\n", area.Name)
+	for _, loc := range areas.Results {
+		fmt.Println(loc.Name)
 	}
 
 	return nil
